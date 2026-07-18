fe4e0fd94496273ddc2a63983748515cabebc04d
Nitric <mail@nitric.me>
2026-07-18 13:26:12 +0900

    chore: initialize Win32 project

---
+++ Common.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <sdkddkver.h>
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+#include <windows.h>
---
+++ Main.cpp
@@ -0,0 +1,6 @@
+#include "Common.h"
+
+int __stdcall wWinMain(HINSTANCE hInstance, HINSTANCE, wchar_t* commandLine, int showCommand)
+{
+    return 0;
+}
